a3bc1107e2c7f0744129f98c8124d489727b0a9d
rustavellyne <rustavellyne@meta.ua>
2020-04-13 21:03:32 +0300

    initial commit

---
+++ app/code/Unit1/Task1/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Task 1  Module registration
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Unit1_Task1', __DIR__);
